84cc7e8030333e68f815512c20cd06ea5e3f829f
Chiragdbb <schirag414@gmail.com>
2023-02-08 03:33:41 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## This project is a clone of Backstage Talks Magazine page which is made in Tailwindcss.
+
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        "Roboto": "Roboto, sans-serif" 
+      },
+      colors: {
+        "bg-white": "#fff",
+        "bg-lightBlue": "#00c1b5",
+        "bg-orange": "#ff651a",
+        "bg-yellow": "#ffbe00",
+        "bg-darkBlue": "#1d3fbb",
+        "bg-red": "#e30512",
+        "text-pink":"#e568ac",
+        "text-black":"#251d20"
+      }
+    },
+  },
+  plugins: [],
+}
