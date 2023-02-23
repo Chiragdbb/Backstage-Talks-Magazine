915a22cd83a7b3e4ec1456411212dabf95cbaaa9
Chiragdbb <schirag414@gmail.com>
2023-02-24 05:27:08 +0530

    added js file

---
+++ script.js
@@ -0,0 +1,19 @@
+// bg-color should be changed according to selcted block
+// scrolling to a block also changes its bg-color
+// scrollbar should not be visible
+
+let bgChange = document.querySelector(".wrapper");
+
+let link6 = document.querySelector("#link-6");
+let link5 = document.querySelector("#link-5");
+let link4 = document.querySelector("#link-4");
+let link3 = document.querySelector("#link-3");
+let link2 = document.querySelector("#link-2");
+let link1 = document.querySelector("#link-1");
+
+link6.onclick = () => (bgChange.style.backgroundColor = "#fff");
+link5.onclick = () => (bgChange.style.backgroundColor = "#00c1b5");
+link4.onclick = () => (bgChange.style.backgroundColor = "#ff651a");
+link3.onclick = () => (bgChange.style.backgroundColor = "#ffbe00");
+link2.onclick = () => (bgChange.style.backgroundColor = "#1d3fbb");
+link1.onclick = () => (bgChange.style.backgroundColor = "#e30512");
